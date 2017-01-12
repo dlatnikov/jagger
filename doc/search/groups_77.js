3b37778260ee3ad91e42b7044f2fdc0491f82cf8
dlatnikov <dlatnikov@griddynamics.com>
2017-01-12 12:36:47 +0300

    JFG-993. CI, Test data storage, report generation

--- doc/search/groups_77.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['workaround_20for_20linking',['Workaround for linking',['../group__Workaround__InvocationInfo__group.html',1,'']]],
-  ['workaround_20for_20linking',['Workaround for linking',['../group__Workaround__InvocationListener__group.html',1,'']]]
-];
